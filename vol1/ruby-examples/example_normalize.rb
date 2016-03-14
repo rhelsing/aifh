ec6905b86b88a84d5918806e4b1a160894415da2
Ryan Helsing <ryanhelsing@gmail.com>
2016-03-13 22:17:45 -0400

    normalize

--- vol1/ruby-examples/example_normalize.rb
+++ vol1/ruby-examples/example_normalize.rb
@@ -4,7 +4,7 @@ require_relative 'lib/normalize'
 normalizer = Normalize.new(File.open("datasets/iris.csv"))
 
 # Setup the first four fields to "range normalize" between -1 and 1, last field to equilateral class encoding.
-normalizer.normailize_based_on_types(["numeric","numeric","numeric","numeric","class"], -1.0, 1.0)
+normalizer.normailize_based_on_types(["numeric", "numeric", "numeric", "numeric", "class"], -1.0, 1.0)
 
 #display result:
 normalizer.result_set.each{|row| p row}
